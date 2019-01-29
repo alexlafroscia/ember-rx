a0d59597c31b6de84e06057306e0998eadf769ff
Alex LaFroscia <alex@lafroscia.com>
2019-01-28 23:12:13 -0800

    fix: remove original source of `subscribe` decorator
    This was left over from a previous pass and I forgot to actually delete the
    commented-out code!

--- addon/decorators/subscribe.js
+++ addon/decorators/subscribe.js
@@ -1,52 +1,6 @@
 import { get, set } from "@ember/object";
 import { assert } from "@ember/debug";
 
-// const subscribe = computedDecoratorWithRequiredParams(
-//   (descriptor, dependentKeys) => {
-//     debugger;
-
-//     assert(
-//       "Only one dependent key can be provided to `subscribe`",
-//       dependentKeys.length === 1
-//     );
-
-//     const { key } = descriptor;
-//     const [observablePropertyName] = dependentKeys;
-
-//     let unsubscribe, lastValue, lastObservable;
-
-//     return computed(observablePropertyName, function() {
-//       const observable = this.get(observablePropertyName);
-
-//       assert(
-//         `Could not find obserable at \`${observablePropertyName}\``,
-//         observable
-//       );
-
-//       // This method can be called when the observable changes _or_ when
-//       // the observable emits a new value. We only want to do this logic
-//       // if the observable changed
-//       if (observable !== lastObservable) {
-//         lastObservable = observable;
-
-//         // Clean up we already have a subscription
-//         if (unsubscribe) {
-//           unsubscribe();
-//         }
-
-//         const subscription = observable.subscribe(value => {
-//           lastValue = value;
-//           this.notifyPropertyChange(key);
-//         });
-
-//         unsubscribe = subscription.unsubscribe.bind(subscription);
-//       }
-
-//       return lastValue;
-//     });
-//   }
-// );
-
 export default function subscribe(observableKey) {
   assert(
     "Must be passed a property to listen to",
